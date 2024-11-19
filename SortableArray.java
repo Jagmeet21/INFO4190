63895c6fc20a49668a7c34a85c88aaa3ffde381e
Jagmeet21 <meetgills21@gmail.com>
2024-11-18 21:20:18 -0800

    Update SortableArray.java

--- SortableArray.java
+++ SortableArray.java
@@ -13,7 +13,7 @@ public class SortableArray {
         Arrays.sort(array);
         return array;
     }
-
+\\test
     public static void main(String[] args) {
         int[] numbers = {5, 2, 9, 1, 3};
         System.out.println("Sorted Integers: " + Arrays.toString(sortIntegers(numbers)));
