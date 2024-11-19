ba05334c31a6916a5c66fe8706f7c20848de0ce7
Jagmeet21 <meetgills21@gmail.com>
2024-11-18 20:57:16 -0800

    code

---
+++ TestSortableArray.java
@@ -0,0 +1,16 @@
+public class SortableArrayTest {
+
+    @Test
+    public void testSortIntegers() {
+        int[] input = {5, 3, 8, 1, 2};
+        int[] expected = {1, 2, 3, 5, 8};
+        assertArrayEquals(expected, SortableArray.sortIntegers(input));
+    }
+
+    @Test
+    public void testSortStrings() {
+        String[] input = {"zebra", "apple", "mango", "banana"};
+        String[] expected = {"apple", "banana", "mango", "zebra"};
+        assertArrayEquals(expected, SortableArray.sortStrings(input));
+    }
+}
